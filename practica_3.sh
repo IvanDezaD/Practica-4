e9eeadfb66d1bd87c7683598449ec540959916af
Sud0Us3r <ivandezad@gmail.com>
2024-04-29 20:30:58 +0200

    Finished script and added the auxiliar script

--- practica_3.sh
+++ practica_3.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+#837603
+
 
 ROJO="\e[0;31m"
 
 #Si el verbose esta activado imprime [LOG] "<info>" donde info es el primer parametro
 log(){  if [[ $LOG == 1 ]]; then
-    echo -e "[\e[0;35mLOG\e[0m] $1" >&2
+    echo -e "[\e[0;35m*\e[0m] $1" >&2
   fi
 }
 
 echoerr(){
-  echo -e "$ROJO$1!\e[0m" >&2
+  echo -e "$ROJO[-] $1!\e[0m" >&2
 } 
 
 #La funcion recibe como primer parametro el nombre del usuario a crear y comprueba si existe en el etc passwd.
@@ -18,19 +20,31 @@ isUserAvailable(){
   log "Comprobando que el usuario $1 esta disponible"
   isAvailable="True"
   
-  grep ^$1 /etc/passwd #Ejecutamos el comando y si es exitoso, el codigo de estado sera 0 con lo cual si es diferente de 0 esto es que ha ido mal
+  grep ^$1 /etc/passwd &>/dev/null #Ejecutamos el comando y si es exitoso, el codigo de estado sera 0 con lo cual si es diferente de 0 esto es que ha ido mal
 
   if [[ $? -eq 0 ]]; then
     local isAvailable="False" #Asi que devolveremos false (aqui cambiamos el valor de la variable de retorno)
-    echoerr "El usuario $1 ya existe"
+  else
+    log "El usuario $1 esta disponible para ser a�adido"
   fi
   echo $isAvailable #Para aqui retornarla
+}
+banner() {
+  echo -e "\033[0;34m _   _                ___  _     _ \033[0m"
+  echo -e "\033[0;34m| | | |              / _ \| |   | |\033[0m"
+  echo -e "\033[0;34m| | | |___  ___ _ __/ /_\ \ | __| |\033[0m"
+  echo -e "\033[0;34m| | | / __|/ _ \ '__|  _  | |/ _\` |\033[0m"
+  echo -e "\033[0;34m| |_| \__ \  __/ |  | | | | | (_| |\033[0m"
+  echo -e "\033[0;34m \___/|___/\___|_|  \_| |_/_|\__,_|\033[0m"
+  echo
+  echo "By: Ivan Deza and David Hudrea"
+  echo 
 }
 
 #Imprime el menu de ayuda del script
 help() {
   echo -e "Menu de ayuda para la practica 3 de Administracion de sistemas: "
-  echo -e "./practica3.sh [-a | -s] <fichero> [-v] [-h]"
+  echo -e "./practica3.sh [-a[v] | -s[v]] <fichero>  [-h]"
   echo -e "-a       Crea los usuarios de la lista."
   echo -e "-s       Borrar los usuarios de la lista <Danger>"
   echo -e "-v       Activa el modo verbose donde se pueden ver los logs de el programa"
@@ -42,11 +56,11 @@ makeBackup(){
   local user=$1
   log "Iniciando backup del usuario $user"
   if [ ! -d /extra/backups ]; then
-    mkdir -p /extra/backups
+    mkdir -p /extra/backups #Si no existe el directorio lo creamos
   fi
-  tar -czvf "/extra/backups/$user.bak.gz" -C "/home/$user" -P
+  tar -cvzf "/extra/backups/$user.bak.gz" "/home/$user" &>/dev/null
   if [[ $? != 0 ]]; then 
-    log "Problema realizando el backup a $user, abortando!"
+    echoerr "Problema realizando el backup a $user, abortando!"
     echo "False"
   else
     log "Backup realizado: /home/$user, ahora en: /extra/backups/$user.bak.gz"
@@ -57,46 +71,42 @@ makeBackup(){
 #Recibimos el usuario a eliminar
 delUser() {
   local user=$1
-  local status=$(makeBackup $user)
-  if [[ $status != 0 ]];
+  local status=$(makeBackup "$user")
+  if [[ $status == "False" ]]; then
     echoerr  "[-] Ha habido algun problema eliminando al usuario: $user"
   else
-    deluser "$user"
+    userdel "$user"
     log "Usuario $user borrado"
     rm -rf "/home/$user"
     log "Directorio /home/$user eliminado."
+  fi
 }
 
 #Recibimos 3 parametros, el nombre de usuario, el uid que se le asignara y la contrasña temporal
 addUser() {
   local newUser=$1
   local pass=$3
-  local group=$2
-  log "Añadiendo al usuario $user"
-  useradd "$newUser" #Añadimos al nuevo usuario 
-  log "Usuario, $newUser, creado"
-  usermod -u 1815 "$newUser" #Cambiamos el uid a 1815
-  log "Asignado el uid de: 1815, al usuario $newUser"
-  usermod -aG "$group" "$newUser"
-  mkdir /home/"$newUser"
-  log "Creado el directorio /home/$newUser"
-  cp -r /etc/skel/* /home/$newUser # Copiamos el directorio etc skel al directorio principal del usuario.
-  log "Copiado el directorio /etc/skel/ a /home/$newUser"
+  local fullName=$2
+
+  log "A�adiendo al usuario $user"
+  useradd -m -k /etc/skel -K UID_MIN=1815 -K PASS_MAX_DAYS=30 -c "$fullName" -U "$newUser" 2>/dev/null
+   
+  log "Usuario, $newUser, creado, "
 }
 
 #Recibimos la linea y el numero de linea, si es correcto devolvemos true, si es incorrecto, devolveremos false y mostraremos un error
 checkLineHealth() {
-  local linea=$1
+  local firstParam=$1
   local nLinea=$2
-  log "Comprobando que la linea $nLinea tiene el numero de caracteres correcto"
-  local params=$(wc $linea -w | awk '{print$1}') #Esto es el numero de parametros en la linea del fichero.
-  log "La linea $nLinea tiene: $params parametros."
-  local success="False"
-  if [[ $params -ne 3 ]]; then
-    echoerr "El numero de parametros es incorrecto en la linea $nLinea"
+  log "Comprobando que la linea $nLinea tiene el numero de parametros correcto"
+  local success="True"
+  numParams=$(echo $linea | awk -F "," '{print NF}')
+  if [[ $numParams -ne 3 ]]; then
+    echoerr "El numero de parametros es incorrecto en la linea $nLinea, no se a�adira a este usuario"
     local success="False"
+  else
+    log "La linea $nLinea tiene el numero de parametros correcto."
   fi
-  log "La linea: $nLinea; tiene los parametros correctos."
   echo $success
 }
 
@@ -107,19 +117,20 @@ while getopts ":vash" opt; do
     log "Verbose activado."
     ;;
   a)
-    mode="Delete"
-    log "Borrando los usuarios"
+    mode="Append"
+    log "Anadiendo los usuarios"
     ;;
   s)
-    mode="Append"
-    log "Añadiendo a los usuarios"
+    mode="Delete"
+    log "Borrando a los usuarios"
     ;;
   h)
     help
     exit 0
     ;;
   *)
-    echo "Opcion no valida" >&2
+    echoerr "Opcion no valida" >&2
+    help
     exit 1
     ;;
   esac
@@ -133,8 +144,8 @@ checkId () {
   log "UID: $id"
   
   local isRoot="True"
-  if [[ id -ne 0 ]]; then
-    log "El script no tiene permisos de superusuario."
+  if [[ $id -ne 0 ]]; then
+    echoerr "El script no tiene permisos de superusuario."
     echo -n "False"
   fi
   echo $isRoot
@@ -143,12 +154,13 @@ checkId () {
 #Comprueba que el numero de parametros sea el correcto (2)
 checkParams() {
   log "Comprobando el numero de parametros recibidos"
-  log "Numero de parametros = $#"
-  if [[ "$#" -ne 2]]; then
-    echo "False"
+  log "Fichero recibido: $1"
+  if [[ "$1" == "" ]]; then
     log "Numero de parametros incorrecto, recibidos: $#"
-  else
-    echo "True"
+    echoerr "Parece que no has proporcionado fichero, proporciona uno!"
+    help
+    log "Saliendo"
+    exit 1
   fi
 }
 
@@ -159,22 +171,22 @@ checkHealth(){
   
   if [[ ! -f /etc/passwd ]]; then #Comprobamos que existe el etc passwd
     isHealthy="False"
-    log "El fichero /etc/passwd no existe, contacta con un administrador."
+    echoerr "El fichero /etc/passwd no existe, contacta con un administrador."
   fi
 
   if [[ -d /etc/skel ]]; then #Comprobamos que el directorio etc/skel exista
     isHealthy="False"
-    log "El directorio /etc/skel no existe contacta con un administrador."
+    echoerr "El directorio /etc/skel no existe contacta con un administrador."
   fi
   echo "$isHealthy"
 }
 
-#Funcion main
 main(){
+  banner
   local file=$1
-  log "El fichero es $1"
-  local status=$(checkParams) #Comprobamos que el numero de parametros es correcto
-
+  log "El fichero es $file"
+  checkParams "$file" #Comprobamos que el numero de parametros es correcto
+  log "A�adiendo a los usuarios especificados en el fichero $file"
   local id=$(checkId) #Comprobamos que se ejecuta con permisos de superUsuario
   if [[ $id == "False" ]]; then
     echoerr "[-] El script necesita permisos de superusuario!!"
@@ -183,33 +195,36 @@ main(){
   
   local iter=0
    while IFS= read -r linea; do #Leemos el fichero linea a linea, ahora usando la primera linea deberemos comprobar que el archivo es sano.
-    iter=((iter++))
+    ((iter++))
+    log "Numero de usuario a a�adir: $iter"
+    log "Linea leida del fichero: $linea"
     if [[ $mode == "Append" ]]; then
-      local status=$(checkLineHealth "$linea" $iter)
-      if [[ status == "True" ]]; then
-        local passwd=$(awk '{print$2}' linea) #La contraseña aponer sera sencillamente el tercer parametro
-        local user=$(awk '{print$3}' linea) #Nuestro usuario sera el primer parametro de la linea
-        local group=$(awk '{print$1}' linea) #Nuestro pid sera el segundo parametro de la lista
+        local user=$(echo $linea | awk -F "," '{print$1}') #Nuestro usuario sera el primer parametro de la linea
+        local passwd=$(echo $linea | awk -F "," '{print$2}') #La contraseña aponer sera sencillamente el tercer parametro
+        local fullName=$(echo $linea | awk -F "," '{print$3}') #Nuestro pid sera el segundo parametro de la lista
         
-        if isUserAvailable "$user"; then #Comprobamos que el usuario existe y si es asi lo añadimos
-          log "Usuario a añadir: $user; id del usuario: $id; contraseña temporal del usuario: $passwd"
-          addUser "$user" "$group" "$passwd" #Añadimos al usuario con los parametros correctos
-          log "Usuario: $user; añadido."
-        else
-          echoerr "[-] El usuario $user ya existe"
+        log "Usuario a a�adir es $user, con la contrase�a $passwd y con fullName $fullName"
+        local status=$(checkLineHealth "$linea" $iter)
+        
+        if [[ $status == "True" ]]; then
+          local status=$(isUserAvailable "$user")
+          if [[ $status == "True" ]]; then
+            addUser "$user" "$fullName" "$passwd" #A�adimos al usuario con los parametros correctos
+          else
+            echoerr "El usuario $user ya existe"
+          fi
         fi
+    else
+      local user=$(echo $linea | awk -F "," '{print$1}') #Nuestro usuario sera el primer parametro de la linea 
+      status=$(isUserAvailable "$user")
+      if [[ $status == "False" ]]; then #&>/dev/null; then #Hacemos la misma comprobacion para ver que exista .
+        log "El usuario a borrar es $user"
+        delUser "$user"
+        log "Elimininando al usuario $user"
       else
-        if ! isUserAvailable "$user"; then #Hacemos la misma comprobacion para ver que exista .
-      
-          delUser "$user"
-          log "Elimininando al usuario $user"
-        
-        else
-          echoerr "El usuario $user no existe."  
-        fi
+        echoerr "El usuario $user no existe."
       fi
     fi
-  done
+  done < "$file"
 }
-
-main $2
+main $1
